bc9139eddf8c61bc66aad66cd99ba00487ce77ce
Part1nax777 <serewka-ru@yandex.ru>
2018-10-16 21:44:26 +0300

    add title in index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_15_193117) do
+ActiveRecord::Schema.define(version: 2018_10_15_202132) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
