87e20c18792941af6cc9f0e66a0133cd5bfe8408
Part1nax777 <serewka-ru@yandex.ru>
2018-10-17 22:03:25 +0300

    add js for buttons

--- public/app.js
+++ public/app.js
@@ -1,8 +1,16 @@
+function add_to_cart()
+{
+	alert('Hello from function');
+}
+
+
 function something() 
 {
-	var x = window.localStorage.getItem('bbb');
+	var x = window.localStorage.getItem('aaa');
 	x = x*1+1
-	window.localStorage.setItem('bbb', x);
-	
+	window.localStorage.setItem('aaa', x);
+
+x=1	
 	alert(x);
 }
+
