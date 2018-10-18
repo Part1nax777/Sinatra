8b5bcf764fb4e01fb9832211103b8c25731df57d
Part1nax777 <serewka-ru@yandex.ru>
2018-10-18 21:34:16 +0300

    add function curt_get_number_of_item

--- public/app.js
+++ public/app.js
@@ -1,6 +1,6 @@
 function add_to_cart(id)
 {
-	var key = 'product_' + id
+	var key = 'product_' + id;
 	var x = window.localStorage.getItem(key);
 	x = x * 1 + 1;
 	window.localStorage.setItem(key, x);
@@ -14,4 +14,6 @@ function something()
 	window.localStorage.setItem('aaa', x);	
 	alert(x);
 }
+
+
 
