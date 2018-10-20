e44aaec7782cdc733ed2417bbda6b16205b8bdf7
Part1nax777 <serewka-ru@yandex.ru>
2018-10-20 21:02:44 +0300

    parse order input

--- app.rb
+++ app.rb
@@ -19,5 +19,27 @@ get '/About' do
 end
 
 post '/cart' do
-	erb 'hello'
+	orders_input = params[:orders]
+	@orders = parse_orders_input orders_input
+	erb "hello #{@orders.inspect}"
 end
+
+def parse_orders_input(orders_input)
+
+  s1 = orders_input.split(/,/)
+
+  arr = []
+
+  s1.each do |x|
+        s2 = x.split(/\=/)
+        s3 = s2[0].split(/_/)
+
+        id = s3[1]
+        value = s2[1]
+
+        arr2 = [id, value]
+        arr.push arr2
+  end
+  return arr
+end
+
